52bdc39140783e35db55b1d8a56dd6af5669ee02
Dmitriy <dr.trem86@gmail.com>
2018-08-10 17:48:56 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace :api do
+      namespace :v1 do
+        resources :categories
+        resources :posts
+      end
+  end
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+end
