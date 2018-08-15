3886939b51e7c74406fbc7ddb77b36e6817c12af
Dmitriy <dr.trem86@gmail.com>
2018-08-15 15:52:12 +0300

    add regExp backend

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true, format: { with: /(^)([A-ZА-Я][A-ZА-Яa-zа-я])/, message: "Мінімум 2 слова по 2 літери, перше слово з великої літери"}
+  validates :name, format: { with: /\./, message: "Має включати ‘.’"}
+  validates :name, format: { with: /(\s+)/, message: "Має включати ‘ ’"}
+  validates :name, presence: true, on: :update
+  validates :description, presence: true
+  validates :description, presence: true, on: :update
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true, format: { with: /(^)([A-ZА-Я][A-ZА-Яa-zа-я])/, message: "Мінімум 2 слова по 2 літери, перше слово з великої літери"}
+  validates :name, format: { with: /\./, message: "Має включати ‘.’"}
+  validates :name, format: { with: /(\s+)/, message: "Має включати ‘ ’"}
+  validates :name, presence: true, on: :update
+  validates :content, presence: true
+  validates :content, presence: true, on: :update
+  validates :file, presence: true
 end
